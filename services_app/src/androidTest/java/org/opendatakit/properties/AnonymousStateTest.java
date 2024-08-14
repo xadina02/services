1d8bba09ab71f386dab44876ffa32f93e25ca3e7
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2024-08-14 15:32:36 +0100

    Enhance test coverage for anonymous state (#298)

--- services_app/src/androidTest/java/org/opendatakit/properties/AnonymousStateTest.java
+++ services_app/src/androidTest/java/org/opendatakit/properties/AnonymousStateTest.java
@@ -13,6 +13,9 @@ import org.opendatakit.services.sync.actions.fragments.ChooseSignInTypeFragment;
 import org.opendatakit.services.utilities.UserState;
 import org.opendatakit.utilities.StaticStateManipulator;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AnonymousStateTest {
 
     public final String APP_NAME = "AnonymousStatePropTest";
@@ -55,6 +58,47 @@ public class AnonymousStateTest {
         String isUserAuthenticatedStr = props.getProperty(CommonToolProperties.KEY_IS_USER_AUTHENTICATED);
         assertThat(isUserAuthenticatedStr).isNull();
     }
+
+    @Test
+    public void verifyDefaultGroupProperty() {
+        Context context = getContext();
+        PropertiesSingleton props = getProps(context);
+
+        String defaultGroup = props.getProperty(CommonToolProperties.KEY_DEFAULT_GROUP);
+        assertThat(defaultGroup).isNotNull();
+        assertThat(defaultGroup).isEmpty();
+    }
+
+    @Test
+    public void verifyRolesListProperty() {
+        Context context = getContext();
+        PropertiesSingleton props = getProps(context);
+
+        String rolesList = props.getProperty(CommonToolProperties.KEY_ROLES_LIST);
+        assertThat(rolesList).isNotNull();
+        assertThat(rolesList).isEmpty();
+    }
+
+    @Test
+    public void verifyLastSyncInfoProperty() {
+        Context context = getContext();
+        PropertiesSingleton props = getProps(context);
+
+        String lastSyncInfo = props.getProperty(CommonToolProperties.KEY_LAST_SYNC_INFO);
+        assertThat(lastSyncInfo).isNull();
+    }
+
+    @Test
+    public void verifyAuthenticationTypeProperty() {
+        Context context = getContext();
+        PropertiesSingleton props = getProps(context);
+
+        String authType = props.getProperty(CommonToolProperties.KEY_AUTHENTICATION_TYPE);
+        assertThat(authType).isNotNull();
+        assertThat(authType).isEqualTo("none");
+    }
+
+
 
     @After
     public void clearProperties() {
